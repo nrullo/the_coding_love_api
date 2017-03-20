e1c5dbd18d199eca329a7a075a9682d3b468422d
Nahuel Rullo <nahuelrullo@gmail.com>
2017-03-20 00:44:27 -0300

    hardcoded response (testing purpose)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/tcl.js
+++ routes/tcl.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET random. */
-router.get('/random/', function(req, res, next) {
-  res.send('respond with a resource');
+const express = require('express');
+const router = express.Router();
+const TCL = require('../lib/tcl');
+
+/* POST random. */
+router.post('/random/', function (req, res, next) {
+
+  TCL.random()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
 
 module.exports = router;
